ea01f6b0456f6eba8779560b0a15d585c0bdd113
enricodelazzari <enrico.delazzari@h-farm.com>
2022-06-17 20:38:31 +0200

    wip

--- src/EgdClientServiceProvider.php
+++ src/EgdClientServiceProvider.php
@@ -10,16 +10,8 @@ class EgdClientServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-egd-client')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-egd-client_table')
-            ->hasCommand(EgdClientCommand::class);
+            ->hasConfigFile();
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,10 +11,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'EnricoDeLazzari\\EgdClient\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
@@ -26,11 +22,6 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-egd-client_table.php.stub';
-        $migration->up();
-        */
+        //
     }
 }
